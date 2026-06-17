6544f118c47c00b773d112b1e5d5b5db72740d04
Tuğba Nur Gültekin <tugbanrgultekin@gmail.com>
2026-06-17 06:17:05 +0300

    Clarify finally block comment in try_catch_ornek.java
    Updated finally block comment for clarity.

--- Nesneye Yönelik Programlama/kodlar/I-O Operations and Exception Handling (Giriş-Çıkış İşlemleri ve İstisna Yönetimi)/try_catch_ornek.java
+++ Nesneye Yönelik Programlama/kodlar/I-O Operations and Exception Handling (Giriş-Çıkış İşlemleri ve İstisna Yönetimi)/try_catch_ornek.java
@@ -21,7 +21,7 @@ Bu işlemi bir try-catch-finally bloğu içine alacaksın.
 
 catch bloklarında: Hem yaş hatasını (IllegalArgumentException) hem de olası bir sistem/yazıcı hatasını (IOException) ayrı ayrı yakalayıp ekrana kibar uyarılar yazdıracaksın.
 
-finally bloğunda: Sinema sisteminin her halükarda ekrana "--- İyi Seyirler Dileriz (Sistem Kapatıldı) ---" yazmasını sağlayacaksın.<p>
+finally bloğunda: Sinema sisteminin her halükarda ekrana "--- İyi Seyirler Dileriz (Sistem Kapatıldı) ---" yazmasını sağlayacaksın.</p>
 
 
 
